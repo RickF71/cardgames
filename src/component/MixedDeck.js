0dc8dc51b6db7aa1776d7f5435df71a2412e8e9c
Richard Fleischman <rickf71@gmail.com>
2022-06-13 14:09:59 -0500

    working now, creating a new page do more

--- src/component/MixedDeck.js
+++ src/component/MixedDeck.js
@@ -2,74 +2,72 @@ import React from 'react';
 // import ReactDOM  from 'react-dom/client';
 // import { Button, Container, Col, Row } from 'react-bootstrap';
 import {  Container, Col, Row } from 'react-bootstrap';
-import { fullDecks } from './DeckOfCards';
+// import { fullDecks } from './DeckOfCards';
 
-// var fullDeck = [
-//     {key:'1',sn:'2c',fn:'2_of_clubs'},
-//     {key:'2',sn:'3c',fn:'3_of_clubs'},
-//     {key:'3',sn:'4c',fn:'4_of_clubs'},
-// ];
-    
-//     '5_of_clubs',
-//     '6_of_clubs',
-//     '7_of_clubs',
-//     '8_of_clubs',
-//     '9_of_clubs',
-//     '10_of_clubs',
-//     'jack_of_clubs2',
-//     'queen_of_clubs2',
-//     'king_of_clubs2',
-//     'ace_of_clubs', 
-//     '2_of_hearts',
-//     '3_of_hearts',
-//     '4_of_hearts',
-//     '5_of_hearts',
-//     '6_of_hearts',
-//     '7_of_hearts',
-//     '8_of_hearts',
-//     '9_of_hearts',
-//     '10_of_hearts',
-//     'jack_of_hearts2',
-//     'queen_of_hearts2',
-//     'king_of_hearts2',
-//     'ace_of_hearts',
-//     '2_of_spades',
-//     '3_of_spades',
-//     '4_of_spades',
-//     '5_of_spades',
-//     '6_of_spades',
-//     '7_of_spades',
-//     '8_of_spades',
-//     '9_of_clubs',
-//     '10_of_spades', 
-//     'jack_of_spades2',
-//     'queen_of_spades2',
-//     'king_of_spades2',
-//     'ace_of_spades',
-//     '2_of_diamonds',
-//     '3_of_diamonds',
-//     '4_of_diamonds',
-//     '5_of_diamonds',
-//     '6_of_diamonds',
-//     '7_of_diamonds',
-//     '8_of_diamonds',
-//     '9_of_diamonds',
-//     '10_of_diamonds',
-//     'jack_of_diamonds2',
-//     'queen_of_diamonds2',
-//     'king_of_diamonds2',
-//     'ace_of_diamonds',
-// ]
-// console.log ('<h1>fullDeck</h1>');
-// console.log (fullDeck);
+var fullDeck = [
+    '2_of_clubs',
+    '3_of_clubs',
+    '4_of_clubs',
+    '5_of_clubs',
+    '6_of_clubs',
+    '7_of_clubs',
+    '8_of_clubs',
+    '9_of_clubs',
+    '10_of_clubs',
+    'jack_of_clubs2',
+    'queen_of_clubs2',
+    'king_of_clubs2',
+    'ace_of_clubs', 
+    '2_of_hearts',
+    '3_of_hearts',
+    '4_of_hearts',
+    '5_of_hearts',
+    '6_of_hearts',
+    '7_of_hearts',
+    '8_of_hearts',
+    '9_of_hearts',
+    '10_of_hearts',
+    'jack_of_hearts2',
+    'queen_of_hearts2',
+    'king_of_hearts2',
+    'ace_of_hearts',
+    '2_of_spades',
+    '3_of_spades',
+    '4_of_spades',
+    '5_of_spades',
+    '6_of_spades',
+    '7_of_spades',
+    '8_of_spades',
+    '9_of_clubs',
+    '10_of_spades', 
+    'jack_of_spades2',
+    'queen_of_spades2',
+    'king_of_spades2',
+    'ace_of_spades',
+    '2_of_diamonds',
+    '3_of_diamonds',
+    '4_of_diamonds',
+    '5_of_diamonds',
+    '6_of_diamonds',
+    '7_of_diamonds',
+    '8_of_diamonds',
+    '9_of_diamonds',
+    '10_of_diamonds',
+    'jack_of_diamonds2',
+    'queen_of_diamonds2',
+    'king_of_diamonds2',
+    'ace_of_diamonds',
+]
+console.log ('<h1>fullDeck</h1>');
+console.log (fullDeck);
 
 
 function MixedDeck(props) {
-    const fullDeck = fullDecks.sort((key, sn, fn) => Math.random()-0.5)
+    const fullDeckMixed = fullDeck.sort((fn, index) => Math.random()-0.5)
     // const fullDeck = fullDecks.map((sn, fn) => (
     //     <div> test  </div>
     // ));
-    const randomizedDeck = fullDeck.map((sn, fn, index) => (
+    const randomizedDeck = fullDeckMixed.map((fn, index) => (
         <Col className="bg-primary p-1 m-1">
             <div style={{ color: "black" }}>{index} </div>
             <img height={100} src={'./card-graphics/' + fn + '.png'} />
